c5df23717639617b75317b752a8564c5e1f8b912
Michail Petrov <Gismanus@gmail.com>
2022-08-21 22:23:39 +0300

    first method2

--- 06_object.js
+++ 06_object.js
@@ -1,1 +1,13 @@
-console.log('Hi');
+let rabbit = {};
+rabbit.speak = function(line) {
+    console.log(`Rabbit says: ${line}`);
+}
+rabbit.speak('I\'m alive');
+
+function speak(line){
+    console.log(`${this.type} rabbit says: '${line}'`);
+}
+let whiteRabbit = {type: 'White', speak};
+let hungryRabbit = {type: 'Hungry', speak};
+whiteRabbit.speak('I am late!');
+hungryRabbit.speak('I would eat a carrot know!');
