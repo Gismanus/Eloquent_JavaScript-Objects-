f249d9d74531e179d786fe68e4c54eff49fb701b
Michail Petrov <Gismanus@gmail.com>
2022-08-24 16:58:44 +0300

    new class "GroupIterator"

--- checking/Vec.js
+++ checking/Vec.js
@@ -1,15 +1,15 @@
 class Vec {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
     }
-    plus(other){
+    plus(other) {
         return new Vec(this.x + other.x, this.y + other.y);
     }
     minus(other) {
         return new Vec(this.x - other.x, this.y - other.y);
     }
-    get length(){
+    get length() {
         return Math.sqrt(this.x * this.x + this.y * this.y)
     }
 }
